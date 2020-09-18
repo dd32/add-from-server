dc48d6cb371c026ea6b834d5ff5a18c14f9f23db
Dion Hulse <contact@dd32.id.au>
2020-09-18 12:30:58 +1000

    Cleanup the old version code.

--- old-versions.php
+++ old-versions.php
@@ -2,6 +2,15 @@
 
 class Plugin {
 	function __construct() {
+		global $wp_version;
+
+		$error = sprintf(
+			__( 'This plugin requires WordPress %1$s or greater, and PHP %2$s or greater. You are currently running WordPress %3$s and PHP %4$s. Please contact your website host or server administrator for more information. The plugin has been deactivated.', 'add-from-server' ),
+			MIN_WP,
+			MIN_PHP,
+			$wp_version,
+			phpversion()
+		);
 
 		// Handle activation gracefully with a block screen.
 		if (
@@ -13,30 +22,15 @@ class Plugin {
 			isset( $_REQUEST['plugin'] ) &&
 			PLUGIN == $_REQUEST['plugin']
 		) {
-			die( sprintf(
-				__( 'This plugin requires WordPress %1$s or greater, and PHP %2$s or greater. You are currently running WordPress %3$s and PHP %4$s. Please contact your website host or server administrator for more information. The plugin has been deactivated.', 'add-from-server' ),
-				MIN_WP,
-				MIN_PHP,
-				$GLOBALS['wp_version'],
-				phpversion()
-			) );
+			die( $error );
 		}
 
-		add_action( 'pre_current_active_plugins', array( $this, 'unsupported_plugins_notice' ) );
+		add_action( 'pre_current_active_plugins', function() use( $error ) {
+			printf(
+				'<div class="error"><p><strong>%s</strong>: %s</p></div>',
+				__( 'Add From Server', 'add-from-server' ),
+				$error
+			);
+		} );
 	}
-
-	function unsupported_plugins_notice( $var ) {
-		printf(
-			'<div class="error"><p><strong>%s</strong>: %s</p></div>',
-			__( 'Add From Server', 'add-from-server' ),
-			sprintf(
-				__( 'This plugin requires WordPress %1$s or greater, and PHP %2$s or greater. You are currently running WordPress %3$s and PHP %4$s. Please contact your website host or server administrator for more information. The plugin has been deactivated.', 'add-from-server' ),
-				MIN_WP,
-				MIN_PHP,
-				$GLOBALS['wp_version'],
-				phpversion()
-			)
-		);
-	}
-
 }
